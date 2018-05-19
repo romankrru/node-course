11a2aaa575bfa4dd628a4d9fa656a67f87968e5f
Roman <rm07ru@gmail.com>
2018-05-19 23:54:48 +0300

    Restarting App with nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,22 +1,7 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('str'));
-const filteredArray = _.uniq(['str', 'str', 1, 2, 3, 1, 2, 4])
-console.log(filteredArray)
-
-// console.log('Result:', notes.add(12, 3))
-
-// fs.appendFile(
-//     'greetings.txt',
-//     `Hello ${userName} \n`,
-//     err => {
-//         if (err)
-//             console.log(err);
-//     }
-// );
