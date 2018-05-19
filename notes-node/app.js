3d088cb4804257ec21a4153604b8a0039f715a29
Roman <rm07ru@gmail.com>
2018-05-19 23:48:28 +0300

    Using 3rd Party Modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,11 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(12, 3))
+// console.log(_.isString(true));
+// console.log(_.isString('str'));
+const filteredArray = _.uniq(['str', 'str', 1, 2, 3, 1, 2, 4])
+console.log(filteredArray)
 
-// const userName = os.userInfo().username;
+// console.log('Result:', notes.add(12, 3))
 
 // fs.appendFile(
 //     'greetings.txt',
