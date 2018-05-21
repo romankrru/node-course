955e81d82a0dfa3c64e16ec7763274aa6925ae3b
Roman <rm07ru@gmail.com>
2018-05-22 00:32:27 +0300

    What's makes up an HTTP request

--- wearher-app/app.js
+++ wearher-app/app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBDfNGV1bxI65B30Hf-QZxFDawnyxPv5Hk&address=1301%20lombard%20street%20philadelphia',
     json: true,
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);    
 });
