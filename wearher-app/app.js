64d9ff493dbc335c0f8a391e32d4febbab0be7f6
Roman <rm07ru@gmail.com>
2018-05-22 21:00:24 +0300

    Encoding User Input

--- wearher-app/app.js
+++ wearher-app/app.js
@@ -1,7 +1,20 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+    a: {
+        demand: true, 
+        alias: 'address',
+        describe: 'Addres to fetch wearcher',
+        string: true,
+    },
+}).help().argv;
+
+const address = argv.a;
+const encodedAddress = encodeURIComponent(address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBDfNGV1bxI65B30Hf-QZxFDawnyxPv5Hk&address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBDfNGV1bxI65B30Hf-QZxFDawnyxPv5Hk&address=${encodedAddress}`,
     json: true,
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
