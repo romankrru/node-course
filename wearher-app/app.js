cea0f4f5c422fe066da976d459db2b3a70fa2f51
Roman <rm07ru@gmail.com>
2018-05-22 21:45:33 +0300

    Wiring up Weather Search

--- wearher-app/app.js
+++ wearher-app/app.js
@@ -1,5 +1,6 @@
 const yargs = require("yargs");
 const geocode = require("./geocode/geocode.js");
+const request = require("request");
 
 const argv = yargs
   .options({
@@ -19,3 +20,22 @@ geocode.geocodeAddress(argv.a, (errorMessage, result) => {
     console.log(JSON.stringify(result, undefined, 2));
   }
 });
+
+const key = "927be538d8b2dc4836bd99754c2a74ed";
+// https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+const latitude = "47.2357137";
+const longitude = "39.701505";
+
+request(
+  {
+    url: `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`,
+    json: true
+  },
+  (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log(body.currently.temperature);
+    } else {
+      console.log("Unable to fetch wearher");
+    }
+  }
+);
